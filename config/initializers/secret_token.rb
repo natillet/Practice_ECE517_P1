02680ccdbe0f3da7e042ef8c2c900632bec39def
Elene Trull <endahner@ncsu.edu>
2012-06-05 18:44:52 -0400

    New Ruby on Rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PracticeEce517P1::Application.config.secret_token = 'c8e6f1ca5b36d42eed5fe3e60b5b4359aec212c42756782151b2d22fb03bef50d536105e76dd82490cf92e05bd404e2abedee3bc7045d5206044addc5ff67640'
